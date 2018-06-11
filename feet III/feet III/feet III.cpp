061663b6292080531d348c74f1aca7aa104e7da4
20172104243 <15049115743@163.com>
2018-06-11 16:23:13 +0800

    补全
    课上没来得及写

--- feet III/feet III/feet III.cpp
+++ feet III/feet III/feet III.cpp
@@ -6,10 +6,61 @@
 using namespace std;
 class CFeet
 {
-
+protected:
+	int feet;
+	int inches;
+public:
+	CFeet();
+	void setfeet(int f);
+	void setinches(int inc);
+	virtual void display();
+	int getfeet();
+	int getinches();
 };
+CFeet::CFeet()
+{
+	feet = 0;
+	inches = 0;
+}
+void CFeet::setfeet(int f)
+{
+	feet = f;
+}
+void CFeet::setinches(int inc)
+{
+	inches = inc%12;
+	feet = feet + inc / 12;
+}
+void CFeet::display()
+{
+	cout << "»ùÀà" << feet << "Ó¢³ß" << inches << "Ó¢´ç" << endl;
+}
+int CFeet::getfeet()
+{
+	return feet;
+}
+int CFeet::getinches()
+{
+	return inches;
+}
+class CMyfeet :public CFeet
+{
+public:
+	void display();
+};
+void CMyfeet::display()
+{
+	cout <<"ÅÉÉúÀà"<<feet<< "Ó¢³ß" << inches << "Ó¢´ç" << endl;
+}
+
 int main()
 {
+	CFeet *p;
+	p = new CMyfeet;
+	p->setfeet(3);
+	p->setinches(8);
+	p->display();
+	p->CFeet::display();
     return 0;
 }
 
